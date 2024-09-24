2cd297c7d88f7a0d18d717b4bbe6c6ce3d5c66f3
Omaga David <omagaowi@gmail.com>
2024-09-24 05:19:02 +0100

    posthog

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import posthog from 'posthog-js'
+
+posthog.init('phc_qXtPBOwULIX4OGaB7CLq2wiSuw0upVzUV91GFmgmudc',
+    {
+        api_host: 'https://us.i.posthog.com',
+        person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+    }
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
 )
